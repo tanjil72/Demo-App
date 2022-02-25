edd699603f24c3cf2fdd1a1c560dda1b6f233082
Tanjil Pranto <tanjil.hossain@metadesignsolutions.co.uk>
2022-02-25 18:55:21 +0600

    update

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -1,8 +1,5 @@
 import React from "react";
 
-const DEFAULT_PLACEHOLDER_IMAGE =
-  "https://us.123rf.com/450wm/pavelstasevich/pavelstasevich1811/pavelstasevich181101027/112815900-no-image-available-icon-flat-vector.jpg?ver=6";
-
 const Movie = ({ movie }) => {
   let Releasedate = movie.release_date;
   const ReleaseArray = Releasedate.split("-");
